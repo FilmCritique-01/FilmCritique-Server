5b52b7431284dab159c4312a04ff842f1241b366
jeongju <wnwjdqkr@gmail.com>
2024-06-07 20:05:59 +0900

    Review 모델 수정

--- critique/models.py
+++ critique/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 class Review(models.Model):
     title = models.CharField(max_length=200)
-    rating = models.FloatField
-    review = models.TextField
+    rating = models.FloatField()
+    review = models.TextField()
     date_watched = models.DateField()
     created_at = models.DateTimeField(auto_now_add=True)
     username = models.CharField(max_length=50)
@@ -13,4 +13,4 @@ class Review(models.Model):
     def __str__(self):
         return self.title
     
-    
+
